5db4d43997f93a6af3df2564f6eccad78b3c72a0
Adda1321 <adilmustafa13@gmail.com>
2024-08-02 17:13:25 +0500

    Auth0 with GraphQL jwt header added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,8 +8,8 @@ import { setContext } from "@apollo/client/link/context";
 import { useAuth0 } from "@auth0/auth0-react";
 import { createHttpLink } from '@apollo/client';
 
-const auth0Domain = "YOUR-AUTH0-DOMAIN"
-const auth0ClientId = "YOUR-AUTH0-CLIENT-ID"
+const auth0Domain = "dev-hy75ehsplicag2bb.us.auth0.com"
+const auth0ClientId = "wn7UNmIn3jaqjRoYF6h7eFU3oO4lxFFr"
 const auth0Audience = "example-blog-hasura"
 
 const hasuraUri = "http://localhost:8080/v1/graphql"
